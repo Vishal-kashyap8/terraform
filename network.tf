bcdaa6370e8fb46f1799ad64b8578b7f007e46c2
Vishal-kashyap8 <48279347+Vishal-kashyap8@users.noreply.github.com>
2024-11-06 01:11:40 +0530

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,24 @@
+/*Network Security group creation*/
+
+resource "azurerm_network_security_group" "NSG1" {
+    name = "nsg1"
+    resource_group_name = "myGroup"
+    location = "North Europe"
+    depends_on = [ azurerm_resource_group.myRG ]
+}
+
+/*Network Security group Rule defination*/
+resource "azurerm_network_security_rule" "Rule1" {
+    name = "rule1"
+    priority = 100
+    direction = "Outbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "*"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+    resource_group_name = "myGroup"
+    network_security_group_name  = "nsg1"
+    
+}
